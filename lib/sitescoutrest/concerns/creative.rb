cb01c33a25dc477325f81fa4430fd7fba6633946
Eric Vass <ericrvass@gmail.com>
2014-04-07 10:43:09 -0400

    Add Creative Asset to Campaign

--- lib/sitescoutrest/concerns/creative.rb
+++ lib/sitescoutrest/concerns/creative.rb
@@ -10,6 +10,24 @@ module Sitescoutrest
         return result
       end
       
+      def create_creative(creative_data, options = {})
+        oauth_token = self.oauth_token || options[:oauth_token]
+        advertiser_id = self.advertiser_id || options[:advertiser_id]
+        path = "/advertisers/#{advertiser_id}/creatives"
+        content_type = 'application/json'
+        result = data_post(path, content_type, creative_data, oauth_token)
+        return result
+      end
+      
+      def add_creative_to_campaign(campaign_id, creative_data, options = {})
+        oauth_token = self.oauth_token || options[:oauth_token]
+        advertiser_id = self.advertiser_id || options[:advertiser_id]
+        path = "/advertisers/#{advertiser_id}/campaigns/#{campaign_id}/creatives"
+        content_type = 'application/json'
+        result = data_post(path, content_type, creative_data, oauth_token)
+        return result
+      end
+      
     end
   end
 end
